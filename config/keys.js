7ed5a6a1a65e43f8bd64f7a21cc1c7d5293a4225
vickeyorton <vigneshmooorthi@gmail.com>
2020-02-26 17:07:28 +0530

    MongoDB config

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://vicky:vicky@cluster0-ykpzz.mongodb.net/test?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB key config
+const db = require("./config/keys").mongoURI;
+
+// Connecting MongoDb
+mongoose
+    .connect(db)
+    .then(()=> console.log('Mongo-DB Get Connected !....'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("Hello"));
 
